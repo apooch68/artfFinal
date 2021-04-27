3891dbb8f1136bbb2b3ce5eadeefb8cdbf39a6fe
Selena Nguyen <selenamiminguyen@gmail.com>
2021-04-27 19:42:36 -0400

    add tile components

---
+++ artf-final/src/PostComponent.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Gallery from './Gallery';
+import PinchableBox from './PinchableBox';
+
+function App() {
+  return (
+    <div></div>
+  );
+}
+
+export default App;
